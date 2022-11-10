4151744bf5a84e5ecd6724f74baa83e9bc99e8fb
Hikaru Takemoto <h.take1320@gmail.com>
2022-11-10 12:55:14 +0900

    未使用変数・関数追加

--- step1/src/hello.js
+++ step1/src/hello.js
@@ -2,10 +2,17 @@ import { sleep } from "./sleep";
 
 /** とりあえずhello */
 export const hello = async () => {
+    // tree-shakingで消えるハズ
+    const unusedVar = "hoge";
+
     console.log("🍣 > hello!");
 
     // ３秒間をおいてから返事
     await sleep(3000);
 
     console.log("🍺 > hello!!");
+};
+
+export const unusedMethod = async () => {
+    console.log("🍣 > unused!");
 };
